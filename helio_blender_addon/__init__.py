8f2814016950b3c2f4de43647ba9e16c2609bd20
Michael Weibel <michael@helio.exchange>
2023-07-19 11:11:11 +0200

    version 0.1.5

--- helio_blender_addon/__init__.py
+++ helio_blender_addon/__init__.py
@@ -27,7 +27,7 @@ from helio_blender_addon import addon
 bl_info = {
     "name": "Helio Cloud Rendering",
     "blender": (3, 20, 0),
-    "version": (0, 1, 4),
+    "version": (0, 1, 5),
     "category": "Render",
     "tracker_url": "https://github.com/helio/blender-addon/issues"
 }
